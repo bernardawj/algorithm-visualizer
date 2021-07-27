c90493943ebe333bad4e9704220b279117acf6b2
Bernard Ang <work@bernardawj.com>
2021-07-27 22:23:05 +0800

    Added animations for insertion sort

--- assets/js/algorithms/sorting-algorithms/insertion-sorting-algorithm.js
+++ assets/js/algorithms/sorting-algorithms/insertion-sorting-algorithm.js
@@ -1,4 +1,6 @@
 import { SortingAlgorithm } from "./sorting-algorithm.js";
+import { Animation } from "../../animators/animation.js";
+import { action } from "../../animators/animation-action.js";
 
 export class InsertionSortingAlgorithm extends SortingAlgorithm {
 
@@ -6,33 +8,35 @@ export class InsertionSortingAlgorithm extends SortingAlgorithm {
         super();
     }
 
-    static sort(array, descendingOrder = false) {
-        // One of the slowest sorting algorithm that compares values with its predecessor to determine its position within the array
+    static sort(animator, array, descendingOrder = false) {
+        // One of the slowest sorting algorithm that compares values with its predecessor to determine its position
+        // within the array
         // Time complexity = O(n^2)
 
         if (!descendingOrder) {
             for (let i = 0; i < array.length; i++) {
                 let j = i;
+                animator.addAnimation(new Animation(action.SEARCH, [i]));
+
                 while (j > 0) {
                     if (array[j] < array[j - 1]) {
-                        InsertionSortingAlgorithm.swap(array, j, j - 1);
+                        InsertionSortingAlgorithm.swap(animator, array, j, j - 1);
                         j--;
-                    }
-                    else {
+                    } else {
                         break;
                     }
                 }
             }
-        }
-        else {
+        } else {
             for (let i = array.length - 1; i >= 0; i--) {
                 let j = i;
+                animator.addAnimation(new Animation(action.SEARCH, [i]));
+
                 while (j < array.length) {
                     if (array[j] < array[j + 1]) {
-                        InsertionSortingAlgorithm.swap(array, j, j + 1);
+                        InsertionSortingAlgorithm.swap(animator, array, j, j + 1);
                         j++;
-                    }
-                    else {
+                    } else {
                         break;
                     }
                 }
