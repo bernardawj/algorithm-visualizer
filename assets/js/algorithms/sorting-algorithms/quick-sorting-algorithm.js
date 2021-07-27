b0a8905de1586b9a2f98cc2fb3002ab90c681c85
Bernard Ang <work@bernardawj.com>
2021-07-27 22:53:09 +0800

    Added animations for quick sort

--- assets/js/algorithms/sorting-algorithms/quick-sorting-algorithm.js
+++ assets/js/algorithms/sorting-algorithms/quick-sorting-algorithm.js
@@ -1,4 +1,6 @@
 import { SortingAlgorithm } from "./sorting-algorithm.js";
+import { Animation } from "../../animators/animation.js";
+import { action } from "../../animators/animation-action.js";
 
 export class QuickSortingAlgorithm extends SortingAlgorithm {
 
@@ -6,28 +8,28 @@ export class QuickSortingAlgorithm extends SortingAlgorithm {
         super();
     }
 
-    static sort(array, descendingOrder = false) {
+    static sort(animator, array, descendingOrder = false) {
         // One of the quickest sorting algorithm that uses divide and conquer to split its values up to sub-arrays and compares values within the sub array
         // using the pivot point to determine the placement of each value
         // Time complexity = O(nLog(n))
 
-        QuickSortingAlgorithm.split(array, descendingOrder);
+        QuickSortingAlgorithm.split(animator, array, descendingOrder);
     }
 
-    static split(array, descendingOrder, low = 0, high = array.length) {
+    static split(animator, array, descendingOrder, low = 0, high = array.length) {
         // Base condition
         if (low >= high)
             return;
 
         // Find the pivoting point
-        const pivot = QuickSortingAlgorithm.partition(array, descendingOrder, low, high);
+        const pivot = QuickSortingAlgorithm.partition(animator, array, descendingOrder, low, high);
 
         // Break down the array internally
-        QuickSortingAlgorithm.split(array, descendingOrder, low, pivot);
-        QuickSortingAlgorithm.split(array, descendingOrder, pivot + 1, high);
+        QuickSortingAlgorithm.split(animator, array, descendingOrder, low, pivot);
+        QuickSortingAlgorithm.split(animator, array, descendingOrder, pivot + 1, high);
     }
 
-    static partition(array, descendingOrder, low, high) {
+    static partition(animator, array, descendingOrder, low, high) {
         const pivot = array[low];
         let i = low, j = high;
 
@@ -35,6 +37,7 @@ export class QuickSortingAlgorithm extends SortingAlgorithm {
             // Find the pivoting point where j intersects with i
             let iCondition;
             do {
+                animator.addAnimation(new Animation(action.SEARCH, [i]));
                 i++;
                 iCondition = !descendingOrder ? array[i] <= pivot : array[i] >= pivot;
             }
@@ -43,18 +46,19 @@ export class QuickSortingAlgorithm extends SortingAlgorithm {
             let jCondition;
             do {
                 j--;
+                animator.addAnimation(new Animation(action.SEARCH, [j]));
                 jCondition = !descendingOrder ? array[j] > pivot : array[j] < pivot;
             }
             while (jCondition);
 
             // Swap the value when it reaches its intersecting point
             if (i < j) {
-                QuickSortingAlgorithm.swap(array, i, j);
+                QuickSortingAlgorithm.swap(animator, array, i, j);
             }
         }
 
         // Swap the pivot point
-        QuickSortingAlgorithm.swap(array, low, j);
+        QuickSortingAlgorithm.swap(animator, array, low, j);
 
         return j;
     }
