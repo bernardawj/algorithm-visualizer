5c85f1118b94444f672c18547a276477c2d42648
Bernard Ang <work@bernardawj.com>
2021-07-27 22:43:51 +0800

    Added animations for selection sort

--- assets/js/algorithms/sorting-algorithms/selection-sorting-algorithm.js
+++ assets/js/algorithms/sorting-algorithms/selection-sorting-algorithm.js
@@ -1,4 +1,6 @@
 import { SortingAlgorithm } from "./sorting-algorithm.js";
+import { Animation } from "../../animators/animation.js";
+import { action } from "../../animators/animation-action.js";
 
 export class SelectionSortingAlgorithm extends SortingAlgorithm {
 
@@ -6,7 +8,7 @@ export class SelectionSortingAlgorithm extends SortingAlgorithm {
         super();
     }
 
-    static sort(array, descendingOrder = false) {
+    static sort(animator, array, descendingOrder = false) {
         // A sorting algorithm that selects the smallest/biggest value (depending on what operation you are doing)
         // and moves it from an unsorted subarray to a sorted subarray
         // Time complexity = O(n^2)
@@ -15,10 +17,16 @@ export class SelectionSortingAlgorithm extends SortingAlgorithm {
             for (let i = 0; i < array.length; i++) {
                 let min = array[i];
                 let index = i;
+                animator.addAnimation(new Animation(action.SEARCH, [i]));
 
                 // Find the min value of the unsorted subarray
                 for (let j = i; j < array.length; j++) {
+                    animator.addAnimation(new Animation(action.SEARCH, [j]));
+
                     if (array[j] < min) {
+                        // Add animation based on
+                        animator.addAnimation(new Animation(action.FOUND_MINIMUM, [index, j])); // Previous index, new index
+
                         // Update minimum value and index
                         min = array[j];
                         index = j;
@@ -26,17 +34,23 @@ export class SelectionSortingAlgorithm extends SortingAlgorithm {
                 }
 
                 // Swap the value from the unsorted subarray to the sorted subarray
-                SelectionSortingAlgorithm.swap(array, i, index);
+                SelectionSortingAlgorithm.swap(animator, array, i, index);
             }
         }
         else {
             for (let i = array.length - 1; i >= 0; i--) {
                 let min = array[i];
                 let index = i;
+                animator.addAnimation(new Animation(action.SEARCH, [i]));
 
                 // Find the min value of the unsorted subarray
                 for (let j = i; j >= 0; j--) {
+                    animator.addAnimation(new Animation(action.SEARCH, [j]));
+
                     if (array[j] < min) {
+                        // Add animation based on
+                        animator.addAnimation(new Animation(action.FOUND_MINIMUM, [index, j])); // Previous index, new index
+
                         // Update minimum value and index
                         min = array[j];
                         index = j;
@@ -44,7 +58,7 @@ export class SelectionSortingAlgorithm extends SortingAlgorithm {
                 }
 
                 // Swap the value from the unsorted subarray to the sorted subarray
-                SelectionSortingAlgorithm.swap(array, i, index);
+                SelectionSortingAlgorithm.swap(animator, array, i, index);
             }
         }
     }
