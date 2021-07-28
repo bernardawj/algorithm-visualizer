589d333322c23730aefeae8d0c7ec66227f51fa9
Bernard Ang <work@bernardawj.com>
2021-07-28 09:55:41 +0800

    Added additional tests for sorting animator

--- assets/js/test/animators/sorting-animator.test.js
+++ assets/js/test/animators/sorting-animator.test.js
@@ -1,12 +1,15 @@
 import { SortingVisualizer } from "../../components/visualizers/sorting-visualizer";
 import { SortingAnimator } from "../../animators/sorting-animator";
+import { Sidebar } from "../../components/sidebar";
 
 jest.mock("../../components/visualizers/sorting-visualizer");
 jest.mock("../../components/sidebar");
 const visualizer = new SortingVisualizer();
+const sidebar = new Sidebar(visualizer);
 
 beforeEach(() => {
     SortingVisualizer.mockClear();
+    Sidebar.mockClear();
 });
 
 describe("Sorting Animator Class", () => {
@@ -22,5 +25,12 @@ describe("Sorting Animator Class", () => {
         expect(() => {
             animator.animate(visualizer);
         }).toThrow(new Error("Sidebar is not initialized."));
+    });
+
+    test("should not throw an error if sidebar is an instance of Sidebar when passed into animate function", () => {
+       const animator = new SortingAnimator(visualizer);
+       expect(() => {
+           animator.animate(sidebar);
+       }).not.toThrow(new Error("Sidebar is not initialized."));
     });
 });
